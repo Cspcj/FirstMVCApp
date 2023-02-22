b2599312fb6d12eb3cdcc1d9c3a62af57df03f27
Cspcj <calin.serban.spanu@gmail.com>
2023-02-22 21:28:54 +0200

    first session

---
+++ Repositories/AnnouncementsRepository.cs
@@ -0,0 +1,24 @@
+﻿using FirstMVCApp.DataContext;
+using FirstMVCApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstMVCApp.Repositories
+{
+    public class AnnouncementsRepository
+    {
+        // clasele de tip repository sunt clase care implementeaza operatiile CRUD pe Baza de date
+
+        private readonly ProgrammingClubDataContext _context;
+
+        public AnnouncementsRepository(ProgrammingClubDataContext context)
+        {
+            _context= context;
+        }
+
+        // metoda echivalenta Get
+        public DbSet<AnnouncementModel> GetAnnouncements ()
+        {
+            return _context.Announcements;
+        }
+    }
+}
